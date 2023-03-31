f55181176c7608a1d032d89421921f61a3c0573a
Takha65 <mountahambackendoye@gmail.com>
2023-03-31 15:40:11 +0000

    package managing

--- src/App.js
+++ src/App.js
@@ -3,12 +3,15 @@ import './App.css'
 import NavBar from './pages/home/navbar'
 import MotDupresident from './components/motdupresi/motdupresident'
 import Partenaires from './components/partenaires/Partenaires'
-import Commission from './components/commissions/commissions'
+import Commissions from './components/commissions/commissions'
 
 function App() {
   return (
     <div className="App">
-        <Commission/>
+         
+        <Commissions/>
+
+
     </div>
   );
 }
