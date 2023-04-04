3624c4eaa40d3ba8f66050bfaa31f0a0da1b9743
Esprit-Noir <mamadouthiandoum96@gmail.com>
2023-04-04 14:28:26 +0000

    refont Footer

--- src/components/modules/footer.jsx
+++ src/components/modules/footer.jsx
@@ -1,79 +1,130 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Footer = () => {
   return (
-    <div className="w-full h-60">
-      <div className="bg-[#0C1F3C] w-full h-full ml-[1px] px-40 rounded-t-sm text-white text-xs flex items-center justify-between">
-        <ul className=" flex flex-col">
-          <a href="" className="mb-4">
-            lorem Ipsum lorem
-          </a>
-          <a href="" className="mb-4">
-            lorem Ipsum lorem
-          </a>
-          <a href="" className="mb-4">
-            lorem Ipsum lorem
-          </a>
-          <a href="" className="mb-4">
-            lorem Ipsum lorem
-          </a>
-          <a href="" className="">
-            lorem Ipsum lorem
-          </a>
-        </ul>
-        <ul className=" flex flex-col">
-          <a href="" className="mb-4">
-            lorem Ipsum lorem
-          </a>
-          <a href="" className="mb-4">
-            lorem Ipsum lorem
-          </a>
-          <a href="" className="mb-4">
-            lorem Ipsum lorem
-          </a>
-          <a href="" className="mb-4">
-            lorem Ipsum lorem
-          </a>
-          <a href="" className="">
-            lorem Ipsum lorem
-          </a>
-        </ul>
-        <ul className=" flex flex-col">
-          <a href="" className="mb-4">
-            lorem Ipsum lorem
-          </a>
-          <a href="" className="mb-4">
-            lorem Ipsum lorem
-          </a>
-          <a href="" className="mb-4">
-            lorem Ipsum lorem
-          </a>
-          <a href="" className="mb-4">
-            lorem Ipsum lorem
-          </a>
-          <a href="" className="">
-            lorem Ipsum lorem
-          </a>
-        </ul>
-        <ul className=" flex flex-col">
-          <a href="" className="mb-4">
-            lorem Ipsum lorem
-          </a>
-          <a href="" className="mb-4">
-            lorem Ipsum lorem
-          </a>
-          <a href="" className="mb-4">
-            lorem Ipsum lorem
-          </a>
-          <a href="" className="mb-4">
-            lorem Ipsum lorem
-          </a>
-          <a href="" className="">
-            lorem Ipsum lorem
-          </a>
-        </ul>
+    <footer class="bg-[#0C1F3C] w-full h-[400px] flex items-center">
+      <div class="mx-auto w-full max-w-screen-xl px-10 md:px-0">
+        <div class="grid grid-cols-2 md:gap-8 gap-4 md:px-2 px-10  py-6 lg:py-8 md:grid-cols-4">
+          <div>
+            <ul class="text-white font-medium">
+              <li class="mb-4">
+                <Link href="#" class=" hover:underline">
+                  Contact Us
+                </Link>
+              </li>
+              <li class="mb-4">
+                <Link href="#" class="hover:underline">
+                  Ordering & Payment
+                </Link>
+              </li>
+              <li class="mb-4">
+                <Link href="#" class="hover:underline">
+                  Shipping
+                </Link>
+              </li>
+              <li class="mb-4">
+                <Link href="#" class="hover:underline">
+                  Return
+                </Link>
+              </li>
+              <li class="mb-4">
+                <Link href="#" class="hover:underline">
+                  Faq
+                </Link>
+              </li>
+            </ul>
+          </div>
+          <div>
+            <ul class="text-white font-medium">
+              <li class="mb-4">
+                <Link href="#" class="hover:underline">
+                  About Adobe Xd kid
+                </Link>
+              </li>
+              <li class="mb-4">
+                <Link href="#" class="hover:underline">
+                  Work With Us
+                </Link>
+              </li>
+              <li class="mb-4">
+                <Link href="#" class="hover:underline">
+                  Privacy Policy
+                </Link>
+              </li>
+              <li class="mb-4">
+                <Link href="#" class="hover:underline">
+                  Terms & Conditions
+                </Link>
+              </li>
+              <li class="mb-4">
+                <Link href="#" class="hover:underline">
+                  Press Enquiries
+                </Link>
+              </li>
+            </ul>
+          </div>
+          <div>
+            <ul class="text-white font-medium">
+              <li class="mb-4">
+                <Link href="#" class=" hover:underline">
+                  Contact Us
+                </Link>
+              </li>
+              <li class="mb-4">
+                <Link href="#" class="hover:underline">
+                  Ordering & Payment
+                </Link>
+              </li>
+              <li class="mb-4">
+                <Link href="#" class="hover:underline">
+                  Shipping
+                </Link>
+              </li>
+              <li class="mb-4">
+                <Link href="#" class="hover:underline">
+                  Return
+                </Link>
+              </li>
+              <li class="mb-4">
+                <Link href="#" class="hover:underline">
+                  Faq
+                </Link>
+              </li>
+            </ul>
+          </div>
+          <div>
+            <ul class="text-white font-medium">
+              <li class="mb-4">
+                <Link href="#" class="hover:underline">
+                  About Adobe Xd kid
+                </Link>
+              </li>
+              <li class="mb-4">
+                <Link href="#" class="hover:underline">
+                  Work With Us
+                </Link>
+              </li>
+              <li class="mb-4">
+                <Link href="#" class="hover:underline">
+                  Privacy Policy
+                </Link>
+              </li>
+              <li class="mb-4">
+                <Link href="#" class="hover:underline">
+                  Terms & Conditions
+                </Link>
+              </li>
+              <li class="mb-4">
+                <Link href="#" class="hover:underline">
+                  Press Enquiries
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
       </div>
-    </div>
+    </footer>
   );
 };
 
