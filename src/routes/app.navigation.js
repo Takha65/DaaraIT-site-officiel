af9ea1630f6123927a7ce8ea86b67898bbf1b9f0
Takha65 <mountahambackendoye@gmail.com>
2023-04-06 23:44:46 +0000

    gestion navbar

--- src/routes/app.navigation.js
+++ src/routes/app.navigation.js
@@ -1,12 +1,17 @@
 import Events from '../pages/events/Events';
 import GalleryPage from '../pages/gallery/GalleryPage';
 import Home from '../pages/home/Home';
-import { EVENTS_PATH, GALLERY_PATH, HOME_PATH } from './app.paths';
+import Commissions from '../pages/commissions/commissions'
+import { EVENTS_PATH, GALLERY_PATH,COMMISSION_PATH, HOME_PATH } from './app.paths';
 
 export const navigationPaths = [
     {
         path: HOME_PATH,
         element: <Home />
+    },
+    {
+        path: COMMISSION_PATH,
+        element: <Commissions />
     },
     {
         path: EVENTS_PATH,
