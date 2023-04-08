e033bbe091e38f7af1c9044cc6330b4b184a59d8
Takha65 <mountahambackendoye@gmail.com>
2023-04-08 04:04:11 +0000

    logo partenaires

--- src/pages/home/Partenaires.js
+++ src/pages/home/Partenaires.js
@@ -1,6 +1,16 @@
 
 import TitleComponent from "../../components/modules/title";
 import image2 from "../../images/Huawei-logo.png";
+import auf from "../../images/logo_partenaire/AUF.png" 
+import bakeli from "../../images/logo_partenaire/bakeli.png" 
+import colomb from "../../images/logo_partenaire/colomb.png" 
+import itSchool from "../../images/logo_partenaire/itSchool.jpg" 
+import Jokko from "../../images/logo_partenaire/JOKKO.png" 
+import odc from "../../images/logo_partenaire/ODC.png" 
+import peex from "../../images/logo_partenaire/peex.png" 
+import RootSN from "../../images/logo_partenaire/RootSN.jpg" 
+import xarala from "../../images/logo_partenaire/xarala.png" 
+
 
 const Partenaires = ()=> {
     return ( 
@@ -17,19 +27,28 @@ const Partenaires = ()=> {
           <div className="my-[5rem]">
             <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-10">
               <div className="py-4 flex items-center justify-center shadow-lg">
-                <img className="w-[150px]" src={image2} alt="logo" />
+                <img className="w-[150px]" src={auf} alt="logo" />
               </div>
               <div className="py-4 flex items-center justify-center shadow-lg">
-                <img className="w-[150px]" src={image2} alt="logo" />
+                <img className="w-[150px]" src={bakeli} alt="logo" />
               </div>
               <div className="py-4 flex items-center justify-center shadow-lg">
-                <img className="w-[150px]" src={image2} alt="logo" />
+                <img className="w-[150px]" src={colomb} alt="logo" />
               </div>
               <div className="py-4 flex items-center justify-center shadow-lg">
-                <img className="w-[150px]" src={image2} alt="logo" />
+                <img className="w-[150px]" src={itSchool} alt="logo" />
               </div>
               <div className="py-4 flex items-center justify-center shadow-lg">
-                <img className="w-[150px]" src={image2} alt="logo" />
+                <img className="w-[150px]" src={Jokko} alt="logo" />
+              </div>
+              <div className="py-4 flex items-center justify-center shadow-lg">
+                <img className="w-[150px]" src={Jokko} alt="logo" />
+              </div>
+              <div className="py-4 flex items-center justify-center shadow-lg">
+                <img className="w-[150px]" src={Jokko} alt="logo" />
+              </div>
+              <div className="py-4 flex items-center justify-center shadow-lg">
+                <img className="w-[150px]" src={Jokko} alt="logo" />
               </div>
             </div>
           </div>
