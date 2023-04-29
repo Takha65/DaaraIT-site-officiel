da5702cbd4ea3a1c76d2d4ebe09eff8f14c00c4d
Takha65 <mountahambackendoye@gmail.com>
2023-04-29 17:17:44 +0000

    correction sur le lien instagram

--- src/components/modules/sidemenu.jsx
+++ src/components/modules/sidemenu.jsx
@@ -1,22 +1,12 @@
-<<<<<<< HEAD
-import React,{useState} from 'react'
-import './sidemenu.css';
-import {ReactComponent as FacebookIcon} from "../../ressources/icons/facebook.svg"
-import {ReactComponent as InstagramIcon} from "../../ressources/icons/instagram.svg"
-import {ReactComponent as TwitterIcon} from "../../ressources/icons/twitter.svg"
-import {ReactComponent as YoutubeIcon} from "../../ressources/icons/youtube.svg"
-import {ReactComponent as HomeIcon} from "../../ressources/icons/home.svg"
-import {ReactComponent as LinkedInIcon} from "../../ressources/icons/linkedIn2.svg"
-=======
 import React from "react";
 import "./sidemenu.css";
 import { ReactComponent as HomeIcon } from "../../ressources/icons/home.svg";
 import { GrFacebook, GrInstagram, GrLinkedin, GrTwitter } from "react-icons/gr";
->>>>>>> 4e7000d9003fcf269da49f03ecde179a923ab5a4
 
 const SideMenu = () => {
   return (
     <div className="h-screen w-[50px] hidden sm:block fixed top-0 bg-[#0C1F3C] flex items-end">
+      
       <div className="w-full h-full flex items-center justify-between flex-col pb-2">
         <li className="vide"></li>
         <ul className="h-full flex items-center justify-center flex-col">
