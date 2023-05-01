10cb7b80326aed358018112271da18584804ba23
Moussa Alkaly BADJI <moussabadji079@gmail.com>
2023-05-01 22:47:23 +0000

    feat: agrandir les images au click

--- src/components/gallery/ImageItem.jsx
+++ src/components/gallery/ImageItem.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
 
-const ImageItem = ({image}) => {
+const ImageItem = ({image,showImageModal}) => {
   return (
-    <div className='w-full h-48 shadow-xl  animate__animated animate__backInUp animate__delay-0.5s '>
+    <div 
+      onClick={() => showImageModal(image)}
+      className='w-full h-48 shadow-xl  animate__animated animate__backInUp animate__delay-0.5s'
+      style={{cursor:'pointer'}}
+    >
         <img src={require("../../images/"+image)} alt={"ImageItem"} className="w-full h-full object-cover" />
     </div>
   )
--- src/pages/gallery/GalleryPage.jsx
+++ src/pages/gallery/GalleryPage.jsx
@@ -1,4 +1,5 @@
 import React, {cloneElement} from 'react'
+import { createPortal } from 'react-dom';
 import { useState } from 'react';
 import Banner from '../../components/Banner/Banner';
 import TabItem from '../../components/gallery/TabItem';
@@ -9,7 +10,39 @@ import BannerImg from '../../ressources/images/commission.png';
 import 'animate.css';
 import ImageItem from '../../components/gallery/ImageItem';
 
+const ModalImage = ({open, close, children}) => {
+    if(!open){
+        return null
+    }
+    console.log('yes');
+    return createPortal(
+    <div 
+    onClick={close}
+    style={{
+        position:'fixed',
+        zIndex:'999999',
+        top:'0',
+        left:'0',
+        width:'100%',
+        height:'100vh',
+        backgroundColor:'rgba(20,30,40,0.8)',
+        display:'flex',
+        justifyContent:'center',
+        alignItems:'center',
+        cursor:'pointer'
+    }}>
+        <div 
+        className='animate__animated animate__backInUp animate__delay-0.5s'
+        style={{width:'98%', height:'95%'}}
+        >
+            {children}
+        </div>
+    </div>, document.getElementById('root'))
+}
+
 const GalleryPage = () => {
+    const [selectedImage, setSelectedImage] = useState(null)
+    const [openModal, setOpenModal] = useState(false)
     const [currentTab, setCurrentTab] = useState('all')
     const [displayedImages, setDisplayedImages] = useState(galeryTabs.filter(galery => galery.tab.toLowerCase() === currentTab.toLowerCase())[0].images)
 
@@ -19,11 +52,26 @@ const GalleryPage = () => {
         setDisplayedImages(images)
     }
 
+    const showImageModal = (image) => {
+        setOpenModal(true)
+        setSelectedImage(image)
+    }
+
     // !on clone l'element pour pouvoir lui passer des props supplementaires - "currentTab & changeDisplayedImages"
     const ClonedTabItem = ({tab}) => cloneElement(<TabItem tab={tab} />, {currentTab, changeDisplayedImages})
+    const ClonedImageItem = ({image}) => cloneElement(<ImageItem image={image} />, {showImageModal})
     
   return (
     <div>
+        <ModalImage open={openModal} close={() => setOpenModal(false)}>
+                {
+                    selectedImage ? <img 
+                    src={require("../../images/"+selectedImage)} 
+                    alt={"ImageItem"} 
+                    style={{width:'100%', height:'100%', objectFit:'contain'}}
+                     /> : null
+                }
+        </ModalImage>
         <Banner 
             title="Galerie" 
             img={BannerImg}
@@ -42,7 +90,7 @@ const GalleryPage = () => {
                 {
                     <ResourceDataMap
                         resourceData={displayedImages}
-                        resourceItem={ImageItem}
+                        resourceItem={ClonedImageItem}
                         resourceName="image"
                      />
                 }
