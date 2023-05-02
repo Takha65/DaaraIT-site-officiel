0870e22ac604de195f9ac1defd7e6e8de8f814ab
Esprit-Noir <mamadouthiandoum96@gmail.com>
2023-05-02 16:51:53 +0000

    add event details popup

--- src/components/event/Event.js
+++ src/components/event/Event.js
@@ -1,11 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Event.css';
 import { RiShareForwardFill } from 'react-icons/ri';
 import { AiFillCalendar } from 'react-icons/ai';
 import { GrView } from 'react-icons/gr';
+import { FaTimes } from 'react-icons/fa';
 
 const Event = ({ event }) => {
-  console.log(event);
+  const [toggleState, setToggleState] = useState('');
+
+  const truncate = (str) => {
+    return str.length > 10 ? str.substring(0, 200) + '...' : str;
+  };
+
   return (
     <div className="grid-item">
       <div className="headerCard">
@@ -25,11 +31,17 @@ const Event = ({ event }) => {
           <span>WebDev</span>
         </div>
         <img src={event?.image} alt="cardimage" className="cardimage" />
-        <p className="subtitle">{event?.subtitle}</p>
-        <p className="description">{event?.desc}</p>
+        <p className="description">
+          <span style={{ overflow: 'hidden', textOverflow: 'ellipsis' }}>
+            {truncate(event?.desc)}
+          </span>
+        </p>
       </div>
       <div className="cardfooter">
-        <button>En savoir plus</button>
+        <button onClick={() => setToggleState(event?.title)}>
+          En savoir plus
+        </button>
+
         <div className="dateview">
           <span>
             <AiFillCalendar className="icon" />
@@ -39,6 +51,51 @@ const Event = ({ event }) => {
             <GrView className="icon" />
             1.6k vues
           </span>
+        </div>
+      </div>
+      <div
+        className={
+          toggleState === event?.title
+            ? 'event__modal modal__active'
+            : 'event__modal'
+        }
+      >
+        <div className="event__modal-content">
+          <FaTimes
+            className="event__modal-close"
+            onClick={() => setToggleState('')}
+          />
+          <img src={event?.image} alt="cardimage" className="modalcardimage" />
+          <div className="headerCard">
+            <div>
+              <span></span>
+              <h4>{event?.title}</h4>
+            </div>
+            <button>
+              Partager
+              <RiShareForwardFill />
+            </button>
+          </div>
+          <div className="category">
+            <span>Tech</span>
+            <span>Coding</span>
+            <span>WebDev</span>
+          </div>
+          <p className="description">{event?.desc}</p>
+          <div className="cardfooter">
+            <span></span>
+
+            <div className="dateview">
+              <span>
+                <AiFillCalendar className="icon" />
+                Il y a 6 mois
+              </span>
+              <span>
+                <GrView className="icon" />
+                1.6k vues
+              </span>
+            </div>
+          </div>
         </div>
       </div>
     </div>
